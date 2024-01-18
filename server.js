7444f9456c6632b21432fcf0b4f5b3cfef14745f
Taraka Manikanta Batta <47830202+Tharak37@users.noreply.github.com>
2024-01-18 11:33:44 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require("cors"); 
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+const configuration = new Configuration({
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
+
+app.use(express.json());
+app.use(cors());
+const root = path.join(__dirname, 'dist', 'chatbot-app');
+app.get('*' ,function(req, res) {
+  fs.stat(root + req.path, function(err){
+    if(err){
+        res.sendFile("index.html", { root });
+    }else{
+        res.sendFile(req.path, { root });
+    }
+  })
+});
+
+app.post("/chat", async (req, res) => {
+  const { message } = req.body;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [ { role: "user", content: message }],
+  });
+  
+  const Data = completion.data;
+  console.log('Server Data', Data);
+  const reply = Data.choices[0].message.content;
+  console.log('content', Data.choices[0].message.content);
+  res.json({ reply });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
